90e1c70b6d16f5d92c5ca9a1c651c3fb1516cebb
Philipp Brendel <breph@gmx.net>
2010-11-07 13:01:54 +0100

    Working on Reel Navigator integration.

---
+++ Classes/FBReelNavigatorDelegate.h
@@ -0,0 +1,17 @@
+//
+//  FBReelNavigatorDelegate.h
+//  FrameByFrame
+//
+//  Created by Philipp Brendel on 06.11.10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+@class FBReelNavigator;
+
+#pragma mark -
+#pragma mark Reel Navigator Delegate
+@protocol FBReelNavigatorDelegate
+
+- (void) reelNavigatorRequestsSnapshot: (FBReelNavigator *) strip;
+
+@end
