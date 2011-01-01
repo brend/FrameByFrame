8c234c6cb5fa469c8c0ab6482dc4f7c62589bd70
Philipp Brendel <waldrumpus@gmail.com>
2011-01-01 15:46:45 +0100

    Avoid warnings during drag image creation (issue 3148120)

--- Classes/FBReelNavigator(DragDrop).m
+++ Classes/FBReelNavigator(DragDrop).m
@@ -182,32 +182,30 @@ NSString *FFImagesPboardType = @"FFImagesPboardType", *FBIndexesPboardType = @"F
 {
 	NSAssert(cellIndex < [self count], @"Cell index must be smaller than total number of images in strip");
 	
-//	NSRect destRect = NSMakeRect(0, 0, [self cellWidth], [self cellHeight]);
-//	NSImage *dragImage = [[NSImage alloc] initWithSize: destRect.size];
-//	CIImage *cellImage = [self.dataSource reelNavigator: self imageForCellAtIndex: cellIndex];
-//	CGSize cellImageSize = cellImage.extent.size;
-//	
-//	[dragImage lockFocus];
-//	// Draw the image of the cell being dragged
-//	[cellImage drawInRect: destRect fromRect: NSMakeRect(0, 0, cellImageSize.width, cellImageSize.height) operation: NSCompositeSourceOver fraction: 0.75];
-//	
-//	// Draw the number of images being dragged
-//	NSDictionary *textAttributes = [NSDictionary dictionaryWithObjectsAndKeys: [NSColor whiteColor], NSForegroundColorAttributeName, nil];
-//	NSString *text = [NSString stringWithFormat: @"%d", numberOfImages];
-//	NSSize textSize = [text sizeWithAttributes: textAttributes];
-//	float max = MAX(textSize.width, textSize.height);
-//	NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect: NSMakeRect(destRect.size.width - 4 - max, 4, max, max)];
-//	
-//	[[NSColor redColor] setFill];
-//	[circle fill];
-//	
-//	[text drawAtPoint: NSMakePoint(destRect.size.width - 4 - (max + textSize.width) * 0.5, 4) withAttributes: textAttributes];
-//	
-//	[dragImage unlockFocus];
-//	
-//	return [dragImage autorelease];
+	NSRect destRect = NSMakeRect(0, 0, [self cellWidth], [self cellHeight]);
+	NSImage *dragImage = [[NSImage alloc] initWithSize: destRect.size];
+	NSImage *cellImage = [self.dataSource reelNavigator: self thumbnailForCellAtIndex: cellIndex];
+	NSSize cellImageSize = cellImage.size;
 	
-	return [self.dataSource reelNavigator: self thumbnailForCellAtIndex: cellIndex];
+	[dragImage lockFocus];
+	// Draw the image of the cell being dragged
+	[cellImage drawInRect: destRect fromRect: NSMakeRect(0, 0, cellImageSize.width, cellImageSize.height) operation: NSCompositeSourceOver fraction: 0.75];
+	
+	// Draw the number of images being dragged
+	NSDictionary *textAttributes = [NSDictionary dictionaryWithObjectsAndKeys: [NSColor whiteColor], NSForegroundColorAttributeName, nil];
+	NSString *text = [NSString stringWithFormat: @"%d", numberOfImages];
+	NSSize textSize = [text sizeWithAttributes: textAttributes];
+	float max = MAX(textSize.width, textSize.height);
+	NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect: NSMakeRect(destRect.size.width - 4 - max, 4, max, max)];
+	
+	[[NSColor redColor] setFill];
+	[circle fill];
+	
+	[text drawAtPoint: NSMakePoint(destRect.size.width - 4 - (max + textSize.width) * 0.5, 4) withAttributes: textAttributes];
+	
+	[dragImage unlockFocus];
+	
+	return [dragImage autorelease];
 }
 
 @end
