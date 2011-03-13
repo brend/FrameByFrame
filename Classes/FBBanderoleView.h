c39f439286b841284f162ee4315c5798d9942bf9
Philipp Brendel <waldrumpus@gmail.com>
2011-03-13 23:33:28 +0100

    Add sweet banderole for buttons
    Uwe has created some really nice buttons and a backdrop for them.
    The Banderole View tiles that backdrop to fit the window's width.

---
+++ Classes/FBBanderoleView.h
@@ -0,0 +1,18 @@
+//
+//  FBBanderoleView.h
+//  FrameByFrame
+//
+//  Created by Philipp Brendel on 12.03.11.
+//  Copyright 2011 BrendCorp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface FBBanderoleView : NSView 
+{
+@private
+	NSImage *patternImage;
+}
+
+@end
---
+++ Classes/FBBanderoleView.m
@@ -0,0 +1,47 @@
+//
+//  FBBanderoleView.m
+//  FrameByFrame
+//
+//  Created by Philipp Brendel on 12.03.11.
+//  Copyright 2011 BrendCorp. All rights reserved.
+//
+
+#import "FBBanderoleView.h"
+
+
+@implementation FBBanderoleView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+		patternImage = [NSImage imageNamed: @"ButtonBanderole.png"];
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+	[patternImage release];
+	patternImage = nil;
+	
+    [super dealloc];
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	NSSize patternSize = patternImage.size;
+	NSInteger repetitions = ceilf(self.frame.size.width / patternSize.width);
+	NSRect
+		sourceRect = (NSRect) { .origin = NSZeroPoint, .size = patternSize },
+		destRect = (NSRect) { .origin = dirtyRect.origin, .size = patternSize };
+	
+	while (repetitions--) {
+		[patternImage drawInRect: destRect fromRect: sourceRect operation: NSCompositeSourceOver fraction: 1];
+		
+		destRect.origin.x += patternSize.width;
+	}
+}
+
+@end
